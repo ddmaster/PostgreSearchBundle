8aa86432fed998680cb6d15ce4b0a32c4118e60f
«ddmaster» <ddmreg@gmail.com>
2013-02-21 16:47:05 +0300

    Added postgresql functions and type

---
+++ DQL/TsrankFunction.php
@@ -0,0 +1,31 @@
+<?php
+namespace Innowo\SearchBundle\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+/**
+ * TsrankFunction ::= "TSRANK" "(" StringPrimary "," StringPrimary ")"
+ */
+class TsrankFunction extends FunctionNode
+{
+    public $fieldName = null;
+    public $queryString = null;
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->fieldName = $parser->StringPrimary();
+        $parser->match(Lexer::T_COMMA); 
+        $this->queryString = $parser->StringPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 
+        	'ts_rank_cd(' . $this->fieldName->dispatch($sqlWalker) . ', ' .
+            ' to_tsquery(' . $this->queryString->dispatch($sqlWalker) . '))';
+    }
+}
---
+++ DoctrineExtensionsPostgreSearchBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DoctrineExtensions\PostgreSearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DoctrineExtensionsPostgreSearchBundle extends Bundle
+{
+}
