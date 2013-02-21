92f7ec423486e56abb578c235f0c730be3762a26
«ddmaster» <ddmreg@gmail.com>
2013-02-21 16:58:24 +0300

    Fixed indentation

--- Dbal/TsvectorType.php
+++ Dbal/TsvectorType.php
@@ -6,36 +6,36 @@ use Doctrine\DBAL\Platforms\AbstractPlatform;
  
 class TsvectorType extends Type
 {
-	public function getName()
-	{
-		return 'tsvector';
-	}
+    public function getName()
+    {
+        return 'tsvector';
+    }
  
-	public function canRequireSQLConversion()
-	{
-		return true;
-	}
+    public function canRequireSQLConversion()
+    {
+        return true;
+    }
  
-	public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
-	{
-		return "TSVECTOR";
-	}
+    public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return "TSVECTOR";
+    }
  
-	public function convertToPHPValue($value, AbstractPlatform $platform)
-	{
-		return $value;
-	}
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return $value;
+    }
  
-	public function convertToDatabaseValueSQL($sqlExpr, AbstractPlatform $platform)
-	{
-		return sprintf('to_tsvector(%s)', $sqlExpr);
-	}
+    public function convertToDatabaseValueSQL($sqlExpr, AbstractPlatform $platform)
+    {
+        return sprintf('to_tsvector(%s)', $sqlExpr);
+    }
  
-	public function convertToDatabaseValue($value, AbstractPlatform $platform)
-	{
-		if (is_array($value)) {
-			$value = implode(" ", $value);
-		}
-		return $value;
-	}
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (is_array($value)) {
+            $value = implode(" ", $value);
+        }
+        return $value;
+    }
 }
